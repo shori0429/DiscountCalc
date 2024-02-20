1f92171b3ddd158d02490ef3c2ffb46fcfa292f9
shori0429 <cever0429@gmail.com>
2024-02-20 15:42:04 +0900

    ・DiscountCalc.java 　使用していない関数を削除
    ・calc_title.xml
    　数字のみ入力できるように改修
    
    ・TitleFragment.java
    　数字入力後、何もないところをタップしたときにキーボードを隠すように
    　(ResultFragmentと被っているところは反応しないため要調査)

--- app/src/main/java/com/example/discountcalc/Fragments/TitleFragment.java
+++ app/src/main/java/com/example/discountcalc/Fragments/TitleFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,11 +16,13 @@ import androidx.fragment.app.Fragment;
 import com.example.discountcalc.R;
 import com.example.discountcalc.databinding.CalcTitleBinding;
 
-public class TitleFragment extends Fragment {
+public class TitleFragment extends Fragment{
 
     private CalcTitleBinding calcTitleBinding;
 
     OnClickListener _clickListener;
+
+    InputMethodManager inputMethodManager;
     public TitleFragment(){
         super();
     }
@@ -28,6 +31,7 @@ public class TitleFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         calcTitleBinding = CalcTitleBinding.inflate(inflater, container, false);
         Log.i("TitleFragment",getParentFragmentManager().toString());
+        inputMethodManager=(InputMethodManager) calcTitleBinding.getRoot().getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
         return calcTitleBinding.getRoot();
     }
 
@@ -61,9 +65,11 @@ public class TitleFragment extends Fragment {
         try{
             _clickListener=(OnClickListener) context;
         }catch (ClassCastException e){
-            throw new ClassCastException(getActivity().toString()+"must implement");
+            throw new ClassCastException(context+"must implement");
         }
     }
+
+
 
     private void setClickListeners(View view) {
         String text = "Button Click";
@@ -71,11 +77,15 @@ public class TitleFragment extends Fragment {
             Toast.makeText(view.getContext(), text, Toast.LENGTH_SHORT).show();
             Log.i("test", text);
             _clickListener.onClick();
-
+        });
+        view.setOnClickListener(v->{
+            // キーボードを隠す
+            view.setOnClickListener(c-> inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS));
         });
     }
 
     public interface OnClickListener{
         void onClick();
     }
+
 }

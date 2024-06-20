dd778ae636c18716811e132a6b1d624b072807db
shori0429 <shori0429@icloud.com>
2024-06-20 15:47:41 +0900

    ・fragment_original_toolbarの右ボタンid名が統一されたものでなかったため修正 ・タイトルに設置していた設定ボタンを移動処理を一時的に停止(そのまま関連処理も削除するかも) ・CustomToolBarクラスに書いていた処理をToolBarFragmentに直接書き込むことで2回作成される問題点を強引に修正。 (参考コードはActivityクラスの直下に記載していたため処理を変えないといけなかった) CustomToolBarクラスは再度作り直して拡張性を持てるようにする

--- app/src/main/java/com/example/discountcalc/Fragments/ToolBarFragment.java
+++ app/src/main/java/com/example/discountcalc/Fragments/ToolBarFragment.java
@@ -11,6 +11,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.discountcalc.R;
 import com.example.discountcalc.databinding.FragmentOriginalToolbarBinding;
@@ -18,6 +21,9 @@ import com.example.discountcalc.databinding.FragmentOriginalToolbarBinding;
 public class ToolBarFragment extends Fragment implements ToolBarCustomViewDelegate {
 
     private FragmentOriginalToolbarBinding binding;
+
+    boolean isHideLeftButton=false;
+    boolean isHideRightButton=false;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,27 +33,58 @@ public class ToolBarFragment extends Fragment implements ToolBarCustomViewDelega
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding=FragmentOriginalToolbarBinding.inflate(inflater,container,false);
-        setCustomToolBar();
+
+        setCustomToolBar(inflater);
 
         return binding.getRoot();
-
     }
 
-    private void setCustomToolBar(){
-        CustomToolBar customToolBar=new CustomToolBar(this.requireContext());
-        customToolBar.delegate=this;
+    private void setCustomToolBar(LayoutInflater inflater){
+//        CustomToolBar customToolBar=new CustomToolBar(inflater.getContext());
+//        customToolBar.delegate=this;
 
-        String title=getString(R.string.title_tool_bar);
-        customToolBar.configure(title,false,false);
+        String title="バー";
+//        customToolBar.configure(title,false,false);
+        binding.ActionTitle.setText(title);
 
-        // カスタムツールバーを挿入するコンテナを指定
-        LinearLayoutCompat layoutCompat=binding.layoutCustomToolbar;
+        setLeftButton(isHideLeftButton);
 
-        // ツールバーの表示をコンテナに合わせる
-        customToolBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        setRightButton(isHideRightButton);
 
-        // カスタムツールバーを表示する
-        layoutCompat.addView(customToolBar);
+//        // カスタムツールバーを挿入するコンテナを指定
+//        LinearLayoutCompat layoutCompat=binding.layoutCustomToolbar;
+//
+//        // ツールバーの表示をコンテナに合わせる
+//        customToolBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+//
+//        layoutCompat.addView(customToolBar);
+    }
+
+    public void setLeftButton(boolean isVisible){
+        // 左ボタン
+        // ボタンの表示/非表示設定
+        if(isVisible) {
+            binding.ActionLeftButton.setVisibility(View.INVISIBLE);
+        }else{
+            binding.ActionLeftButton.setVisibility(View.VISIBLE);
+        }
+        binding.ActionLeftButton.setOnClickListener(b->{
+            onClickedLeftButton();
+        });
+        //onClickedLeftButton();
+    }
+
+    public void setRightButton(boolean isVisible){
+        // 右ボタン
+        // ボタン表示/非表示設定
+        if(isVisible) {
+            binding.ActionRightButton.setVisibility(View.INVISIBLE);
+        }else{
+            binding.ActionRightButton.setVisibility(View.VISIBLE);
+        }
+        binding.ActionRightButton.setOnClickListener(b->{
+            onClickedRightButton();
+        });
     }
 
     @Override
@@ -58,6 +95,16 @@ public class ToolBarFragment extends Fragment implements ToolBarCustomViewDelega
     @Override
     public void onClickedRightButton() {
         Log.i("ToolBarOnClicked","onClickedRightButton");
-
+        setNavGraphDestination();
     }
+
+    // navigationGraphのDestination遷移を実装
+    private void setNavGraphDestination(){
+        NavHostFragment navHostFragment=(NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.host_fragment);
+        assert navHostFragment != null:"null navHostFragment. TitleFragment.java line:89";
+        NavController navHostController=navHostFragment.getNavController();
+        NavDirections navDirections=TitleFragmentDirections.actionTitleFragmentToSettingsFragment();
+        navHostController.navigate(navDirections);
+    }
+
 }

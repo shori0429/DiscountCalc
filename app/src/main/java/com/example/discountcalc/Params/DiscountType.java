fe739c5c06526a0c69740d3dda67ba8005c6dbfd
shori0429 <shori0429@icloud.com>
2024-12-11 15:49:33 +0900

    ・試験的なブランチ データストアの取得をアクティビティで行うように変更。(仮実装) ResultListFragment.javaの処理が大幅に変更される予定
    ・DiscountTypeのenumの並び順変更
    ・Preferences.xmlのkeyの名前に統一性を持たせる
    ・DataStoreに保持していた使用する設定データを、androidStudioのPreferenceManagerから取得するように変更。(仮作成)

--- app/src/main/java/com/example/discountcalc/Params/DiscountType.java
+++ app/src/main/java/com/example/discountcalc/Params/DiscountType.java
@@ -2,9 +2,8 @@ package com.example.discountcalc.Params;
 
 public enum DiscountType {
     None(-1),
-    Custom(0),
-    Const(1);
-
+    Preset(0),
+    Custom(1);
     private final int value;
     DiscountType(int value){
         this.value=value;

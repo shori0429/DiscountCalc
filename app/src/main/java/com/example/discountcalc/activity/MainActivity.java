981119fa23d608a5c6b6f32b6822b97ec3a6297a
shori0429 <shori0429@icloud.com>
2025-06-25 15:12:02 +0900

    【修正】 ・MainActivityの使用しなくなっていたデータストア周りの処理を削除

--- app/src/main/java/com/example/discountcalc/activity/MainActivity.java
+++ app/src/main/java/com/example/discountcalc/activity/MainActivity.java
@@ -12,18 +12,8 @@ import com.example.discountcalc.dataBase.DataStoreHelper;
 import com.example.discountcalc.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity{
-
-    // 設定ファイル名保存キー("xxx".preferences_pb)
-    private static final String SETTING_DATA = "setting_data";
     private ActivityMainBinding binding;
 
-    // データストア
-    RxDataStore<Preferences> datastoreRX;
-    // データストアのインスタンス取得用
-    CustomConfigDataStoreSingleton dataStoreSingleton;
-
-    // データストアヘルパー取得用
-    private DataStoreHelper dataStoreHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +21,12 @@ public class MainActivity extends AppCompatActivity{
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        // データストアシングルトン取得
-        getDataStoreInstance();
         setContentView(binding.getRoot());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-    }
-
-    // DataStore取得
-    private void getDataStoreInstance() {
-        dataStoreSingleton = CustomConfigDataStoreSingleton.getInstance();
-        // データストアのSingletonが存在してなければ、新たにデータストアを作成
-        if (dataStoreSingleton.getDatastore() == null) {
-            datastoreRX = new RxPreferenceDataStoreBuilder(this, SETTING_DATA).build();
-        } else {
-            // データストア取得
-            datastoreRX = dataStoreSingleton.getDatastore();
-        }
-        // Singletonのデータストアにセット
-        dataStoreSingleton.setDataStore(datastoreRX);
     }
 
 }

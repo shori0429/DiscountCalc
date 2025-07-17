8d87146dbbf9e5bfc44ca077b37b7510aa5e4830
shori0429 <shori0429@icloud.com>
2025-07-17 15:53:58 +0900

    ※現在まともに動作しない。ViewModelのLiveDataにnull以外の値がセットされるのを待ってから処理を開始させたい。調査中 【実装】 ・CustomPreferenceListViewModel.java -LivedataのallPreferenceParamListフィールドを作成。repositoryのpreferenceParamListのobserveをこっちのフィールドに変更。 -setPreferenceParamListメソッドを作成。 allPreferenceParamListから引数の名前と一致する要素を抽出し、preferenceDataListにセット。抽出したデータリストが空であれば初期値を1個セット
    ・PreferenceParam.java
    -createDefaultParamメソッドを作成
    引数なしのメソッドで、初期値のデータを作成したいときに呼び出す。
    
    【修正】
    ・CustomPreferenceListViewModel.java
    -使用していないフィールドを削除

--- app/src/main/java/com/example/discountcalc/viewModels/CustomPreferenceListViewModel.java
+++ app/src/main/java/com/example/discountcalc/viewModels/CustomPreferenceListViewModel.java
@@ -20,20 +20,33 @@ import java.util.stream.Collectors;
 public class CustomPreferenceListViewModel extends AndroidViewModel {
 
     private PreferenceParamRepository dataRepository;
-    //private final MutableLiveData<List<CustomPreferenceData>> preferenceDataList;
     private final MutableLiveData<List<PreferenceParam>> preferenceParamList;
+
+    private final MutableLiveData<List<PreferenceParam>> allPreferenceParamList;
     // リポジトリのLiveData追跡用のフィールド
-//    private final LiveData<List<PreferenceParam>> repoPreferenceParamList;
 
 
     public CustomPreferenceListViewModel(Application application){
         super(application);
         dataRepository=new PreferenceParamRepository(application);
         preferenceParamList=new MutableLiveData<>();
-        dataRepository.PreferenceParamList().observeForever(preferenceParamList::setValue);
-//        repoPreferenceParamList= dataRepository.PreferenceParamList();
+        allPreferenceParamList=new MutableLiveData<>();
+        dataRepository.PreferenceParamList().observeForever(allPreferenceParamList::setValue);
+//      repoPreferenceParamList= dataRepository.PreferenceParamList();
     }
 
+    // 全データから使用データする保存名を抽出してlivedataにセット
+    public void setPreferenceParamList(String name){
+        List<PreferenceParam> allData=allPreferenceParamList.getValue();
+        List<PreferenceParam> dataList=new ArrayList<>();
+        if(allData!=null) {
+            dataList = allData.stream().filter(param -> param.saveName().equals(name))
+                    .collect(Collectors.toList());
+        }
+        // 一致データが存在していなかったら初期値を1個セット。
+        if(dataList.size()==0)dataList.add(PreferenceParam.createDefaultParam());
+        preferenceParamList.setValue(dataList);
+    }
     public LiveData<List<PreferenceParam>> PreferenceParamList(){
         return preferenceParamList;
     }

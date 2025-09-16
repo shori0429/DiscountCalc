634af3b4d841cd4bf1faa015325f120ad10c7e60
shori0429 <cever0429@gmail.com>
2025-09-16 15:42:12 +0900

    【実装】
    【修正】
    ・PreferenceParam.java
    -フィールド
    呼び出す際に使いづらかった為、パラメータの順番をid,per,saveNameに変更。
    -コンストラクタ　
    上記変更に合わせた修正。
    -createDefaultParamメソッド
    idのスタートを1に変更(保存時にはcreatePreferenceParamメソッドを使用し、uidを受け取らず、0で固定いるため)

--- app/src/main/java/com/example/discountcalc/params/PreferenceParam.java
+++ app/src/main/java/com/example/discountcalc/params/PreferenceParam.java
@@ -7,15 +7,15 @@ import androidx.room.PrimaryKey;
 
 @Entity(tableName = "custom_preference_table")
 public record PreferenceParam (@PrimaryKey(autoGenerate = true) int uid,
-                               @ColumnInfo(name = "save_name") String saveName,
-                               @ColumnInfo(name = "per") int per){
+                               @ColumnInfo(name = "per") int per,
+                               @ColumnInfo(name = "save_name") String saveName){
     // id指定をしないことでデータベースのuidをオートインクリメントさせる。
-    public static PreferenceParam createPreferenceParam(String saveName, int per){
-        return new PreferenceParam(0,saveName,per);
+    public static PreferenceParam createPreferenceParam(int per,String saveName){
+        return new PreferenceParam(0,per,saveName);
     }
 
     public static PreferenceParam createDefaultParam(){
-        return new PreferenceParam(0,"",0);
+        return new PreferenceParam(1, 0,"");
     }
 
     @NonNull

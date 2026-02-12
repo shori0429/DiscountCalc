ea2e63972eb8bd1293f97ccbcb86a11cfac364f1
shori0429 <cever0429@gmail.com>
2026-02-12 11:40:14 +0900

    createDefaultParamの呼び出しを修正

--- app/src/main/java/com/example/discountcalc/viewModels/CustomPreferenceListViewModel.java
+++ app/src/main/java/com/example/discountcalc/viewModels/CustomPreferenceListViewModel.java
@@ -48,7 +48,7 @@ public class CustomPreferenceListViewModel extends AndroidViewModel {
                     .collect(Collectors.toList());
         }
         // 一致データが存在していなかったら初期値を1個セット。
-        if (dataList.size() == 0) dataList.add(PreferenceParam.createDefaultParam());
+        if (dataList.size() == 0) dataList.add(PreferenceParam.createDefaultParam(dataList.size()));
         return dataList;
     }
 
@@ -120,7 +120,7 @@ public class CustomPreferenceListViewModel extends AndroidViewModel {
     // リストの全要素削除
     public void allRemovePreferenceData(){
             ArrayList<PreferenceParam> dataList=new ArrayList<>();
-            dataList.add(PreferenceParam.createDefaultParam());
+            dataList.add(PreferenceParam.createDefaultParam(dataList.size()));
             preferenceParamList.postValue(dataList);
     }
 

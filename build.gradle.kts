219928a5f95359e00b91bfba326633cb82566d60
shori0429 <cever0429@gmail.com>
2026-05-12 19:03:11 +0900

    Material3を使う為にcompileSdk=37,gradle-version=9.2.1に変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "9.2.1" apply false
     id("java")
 }
 buildscript{
@@ -9,7 +9,7 @@ buildscript{
 
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.8")
     }
 }
 

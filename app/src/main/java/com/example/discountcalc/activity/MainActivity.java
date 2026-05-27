434af211770f2c202ac1c648c835f9a29d5c4f66
shori0429 <cever0429@gmail.com>
2026-05-27 16:31:37 +0900

    toolBarFragmentフィールドを削除し、OnCreate内のローカル変数としてインスタンスを作成するように変更。

--- app/src/main/java/com/example/discountcalc/activity/MainActivity.java
+++ app/src/main/java/com/example/discountcalc/activity/MainActivity.java
@@ -14,7 +14,6 @@ import com.example.discountcalc.fragments.ToolBarFragment;
 public class MainActivity extends AppCompatActivity{
     private ActivityMainBinding binding;
 
-    ToolBarFragment toolBarFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +24,15 @@ public class MainActivity extends AppCompatActivity{
 
 
         if(savedInstanceState == null){
-            toolBarFragment=new ToolBarFragment();
+            ToolBarFragment toolBarFragment=new ToolBarFragment();
 
             // ツールバーフラグメントを配置
             FragmentManager fragmentManager=getSupportFragmentManager();
             fragmentManager.beginTransaction()
                     .replace(R.id.Main_ToolBarView,toolBarFragment)
                     .commit();
-        }else {
-            toolBarFragment = (ToolBarFragment) getSupportFragmentManager().findFragmentById(R.id.Main_ToolBarView);
         }
+
         // 空白箇所タップでフォーカスを外す(子フラグメントのeditText用)
         binding.getRoot().setOnClickListener(v->{
             InputMethodManager imm=(InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);

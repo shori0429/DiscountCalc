f1a74f6509560bd1f0790fd0b7e2c8ca2325a814
shori0429 <cever0429@gmail.com>
2026-05-27 14:06:14 +0900

    replaceとsetOnClickListenerを使用しているリスナーをOnCreateに移動。

--- app/src/main/java/com/example/discountcalc/activity/MainActivity.java
+++ app/src/main/java/com/example/discountcalc/activity/MainActivity.java
@@ -24,25 +24,28 @@ public class MainActivity extends AppCompatActivity{
         setContentView(binding.getRoot());
 
         toolBarFragment=new ToolBarFragment();
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
 
         // ツールバーフラグメントを配置
         FragmentManager fragmentManager=getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.Main_ToolBarView,toolBarFragment)
                 .commit();
-
         // 空白箇所タップでフォーカスを外す(子フラグメントのeditText用)
         binding.getRoot().setOnClickListener(v->{
             InputMethodManager imm=(InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
             v.clearFocus();
         });
+    }
+
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
     }
 
 }

df760af1174a4798dec21c2ba976b1075b7437f1
shori0429 <cever0429@gmail.com>
2026-05-27 16:24:26 +0900

    globalLayoutListenerの解放漏れ修正

--- app/src/main/java/com/example/discountcalc/fragments/ResultListFragment.java
+++ app/src/main/java/com/example/discountcalc/fragments/ResultListFragment.java
@@ -125,7 +125,6 @@ public class ResultListFragment extends Fragment {
 
     @Override
     public void onDestroyView() {
-        super.onDestroyView();
         if (resultOneCalcLabelView != null && globalLayoutListener != null) {
             resultOneCalcLabelView.getViewTreeObserver().removeOnGlobalLayoutListener(globalLayoutListener);
         }
@@ -135,6 +134,7 @@ public class ResultListFragment extends Fragment {
         resultLayoutAdapter = null;
         resultOneCalcLabelView = null;
         progressBarView = null;
+        globalLayoutListener =null;
 
     }
 

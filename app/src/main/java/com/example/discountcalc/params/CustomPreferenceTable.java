91fa59d9782330fca9f0d960742375a16bb2dd60
shori0429 <cever0429@gmail.com>
2026-06-16 21:01:31 +0900

    プライマリキーにカラム名を定義。orderIndexのカラム名をSQLの命名規則に沿ったものに修正

--- app/src/main/java/com/example/discountcalc/params/CustomPreferenceTable.java
+++ app/src/main/java/com/example/discountcalc/params/CustomPreferenceTable.java
@@ -6,9 +6,11 @@ import androidx.room.PrimaryKey;
 
 // 設定名テーブル
 @Entity(tableName = "preference_table")
-public record CustomPreferenceTable(@PrimaryKey(autoGenerate = true) long preferenceId,
+public record CustomPreferenceTable(@PrimaryKey(autoGenerate = true)
+                                    @ColumnInfo(name = "preference_id")
+                                    long preferenceId,
                                     @ColumnInfo(name = "save_name") String saveName,
-                                    @ColumnInfo(name = "orderIndex") int orderIndex) {
+                                    @ColumnInfo(name = "order_index") int orderIndex) {
     public static CustomPreferenceTable createPreferenceParam(String saveName, int orderIndex) {
         return new CustomPreferenceTable(0, saveName, orderIndex);
     }

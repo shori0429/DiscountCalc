aecdccad86272407e3602e5a7b01434851f2d677
shori0429 <cever0429@gmail.com>
2026-06-17 12:14:25 +0900

    作成したテーブルを使用したDAOを作成

---
+++ app/src/main/java/com/example/discountcalc/DAO/CustomPreferenceTableDAO.java
@@ -0,0 +1,62 @@
+package com.example.discountcalc.DAO;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.discountcalc.params.CustomPreferenceTable;
+import com.example.discountcalc.params.DiscountRateTable;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+* バッググラウンド実行を行う実装メソッドをリポジトリクラスに書く
+* */
+@Dao
+// @Transactionのメソッドは
+public interface CustomPreferenceTableDAO {
+
+    // 保存済みのsave_nameに基づく割引率リストを取得。(外部キー:discount_id)を利用
+    // ASCで昇順に
+    @Query("SELECT * FROM discount_rate_table " +
+            "WHERE preference_id = :preferenceId ORDER BY order_index ASC")
+    LiveData<List<DiscountRateTable>> getRatesByPreferenceId(long preferenceId);
+
+    // 全save_nameの一覧取得
+    // ASCで昇順に
+    @Query("SELECT * FROM preference_table ORDER BY order_index ASC")
+    LiveData<List<CustomPreferenceTable>> getAllPreferences();
+
+    // 割引率の設定名を新規保存
+    @Insert
+    long insertPreference(CustomPreferenceTable table);
+
+    // 割引率リストを新規保存
+    @Insert
+    long insertRates(List<DiscountRateTable> table);
+
+    // 割引率の設定名を上書き更新
+    @Update
+    void updatePreference(CustomPreferenceTable table);
+
+    // save_nameの重複確認
+    @Query("SELECT COUNT(*) FROM preference_table WHERE save_name = :saveName")
+    int countBySaveName(String saveName);
+
+    // save_nameの削除(CASCADEで割引率も自動削除)
+    @Delete
+    void deletePreference(String saveName);
+
+    // 既存の割引率リストを削除(上書き時に使用)
+    @Query("DELETE FROM discount_rate_table WHERE preference_id = :preferenceId")
+    void deleteRateByPreferenceId(long preferenceId);
+
+    //
+    @Query("SELECT * FROM preference_table " +
+            "JOIN discount_rate_table ON preference_table.preference_id = discount_rate_table.preference_id")
+    Map<CustomPreferenceTable,List<DiscountRateTable>> loadCustomPreferenceTableAndDiscountRateTable();
+}

838c7c957fedd289b5773ac4c9bc6fa313aaf146
shori0429 <cever0429@gmail.com>
2026-06-17 12:14:34 +0900

    DAOとViewModelを中継するリポジトリクラスを作成

---
+++ app/src/main/java/com/example/discountcalc/dataBase/CustomPreferenceRepository.java
@@ -0,0 +1,77 @@
+package com.example.discountcalc.dataBase;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.discountcalc.DAO.CustomPreferenceTableDAO;
+import com.example.discountcalc.params.CustomPreferenceTable;
+import com.example.discountcalc.params.DiscountRateTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class CustomPreferenceRepository {
+    private final CustomPreferenceTableDAO dao;
+
+    // DAOにデータベースの読み取り/書き込みメソッドがすべて含まれているため、
+    // リポジトリコンストラクタには、データベース全体でなくDAOが渡される。
+    public CustomPreferenceRepository(Application application){
+
+        AppDataBase db = AppDataBase.getDatabase(application);
+        dao=db.customPreferenceTableDAO();
+        Log.i("database", Objects.requireNonNull(db.getOpenHelper().getDatabaseName()));
+    }
+
+    /// 保存済みのsave_nameに基づく割引率リストを取得。(外部キー:discount_id)を利用。ASCで昇順に
+    /// "SELECT * FROM discount_rate_table " +
+    ///             "WHERE preference_id = :preferenceId ORDER BY order_index ASC"
+    public LiveData<List<DiscountRateTable>> getRatesByPreferenceId(long preferenceId) {
+        return dao.getRatesByPreferenceId(preferenceId);
+    }
+
+    /// 全save_nameの一覧取得。ASCで昇順に
+    /// "SELECT * FROM preference_table ORDER BY order_index ASC"
+    public LiveData<List<CustomPreferenceTable>> getAllPreferences(){
+        return dao.getAllPreferences();
+    }
+
+    /// 割引率の設定名を新規保存
+    public long insertPreference(CustomPreferenceTable customPreference){
+        return dao.insertPreference(customPreference);
+    }
+
+    /// 割引率リストを新規保存
+    public long insertRates(List<DiscountRateTable> rateList){
+        return dao.insertRates(rateList);
+    }
+
+    /// 割引率の設定名を上書き更新
+    public void updatePreference(CustomPreferenceTable customPreference){
+        dao.updatePreference(customPreference);
+    }
+
+    /// save_nameの重複確認
+    /// "SELECT COUNT(*) FROM preference_table WHERE save_name = :saveName"
+    public int countBySaveName(String saveName){
+        return dao.countBySaveName(saveName);
+    }
+
+    public void deletePreference(String saveName){
+        dao.deletePreference(saveName);
+    }
+
+    /// 既存の割引率リストを削除(上書き時に使用)
+    /// "DELETE FROM discount_rate_table WHERE preference_id = :preferenceId"
+    public void deleteRateByPreferenceId(long preferenceId){
+        dao.deleteRateByPreferenceId(preferenceId);
+    }
+
+    ///
+    public Map<CustomPreferenceTable,List<DiscountRateTable>> loadCustomPreferenceTableAndDiscountRateTable(){
+        return dao.loadCustomPreferenceTableAndDiscountRateTable();
+    }
+
+}

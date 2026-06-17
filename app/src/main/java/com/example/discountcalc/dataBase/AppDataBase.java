af6cce61e6c4e2461e10f7a74a6de04122eb1da9
shori0429 <cever0429@gmail.com>
2026-06-17 12:14:54 +0900

    AppDataBaseを新規RSDBに対応したものに変更。versionを2に

--- app/src/main/java/com/example/discountcalc/dataBase/AppDataBase.java
+++ app/src/main/java/com/example/discountcalc/dataBase/AppDataBase.java
@@ -6,15 +6,20 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.discountcalc.DAO.CustomPreferenceTableDAO;
 import com.example.discountcalc.DAO.PreferenceParamDAO;
-import com.example.discountcalc.params.PreferenceParam;
+import com.example.discountcalc.params.CustomPreferenceTable;
+import com.example.discountcalc.params.DiscountRateTable;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {PreferenceParam.class},version = 1)
+@Database(entities = {CustomPreferenceTable.class,DiscountRateTable.class},version = 2)
 public abstract class AppDataBase extends RoomDatabase {
     public abstract PreferenceParamDAO preferenceParamDAO();
+
+    public abstract CustomPreferenceTableDAO customPreferenceTableDAO();
+    public abstract DiscountRateTable discountRateTable();
 
     private static volatile AppDataBase instance;
     private static final int NUMBER_OF_THREADS=4;
@@ -24,10 +29,13 @@ public abstract class AppDataBase extends RoomDatabase {
         if(instance==null){
             synchronized (AppDataBase.class){
                 if(instance==null){
-                    instance= Room.databaseBuilder(context.getApplicationContext(), AppDataBase.class,DataBaseStrings.DataBaseName).build();
+                    // 第三引数は端末内に保存されるファイル名になる
+                    instance= Room.databaseBuilder(context.getApplicationContext(), AppDataBase.class,"discount-calc_database")
+                            .build();
                 }
             }
         }
         return instance;
     }
+
 }
